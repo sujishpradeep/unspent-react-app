5e52aaa4eb56613b38948e8ee83e68cef58b6b6d
Sujish Pradeep <sujishp164@gmail.com>
2019-09-27 12:37:38 +1000

    Changed port number for AWS test

---
+++ build/precache-manifest.9de205b6daffb728e76de55698c3b286.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae203e8ea0943492b1bcfe3a19f2c41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d06ded5c78d45920cb72",
+    "url": "/static/css/2.bdb7ec01.chunk.css"
+  },
+  {
+    "revision": "fad896a0d77e045dfe61",
+    "url": "/static/css/main.d4fa1708.chunk.css"
+  },
+  {
+    "revision": "d06ded5c78d45920cb72",
+    "url": "/static/js/2.5c02e836.chunk.js"
+  },
+  {
+    "revision": "fad896a0d77e045dfe61",
+    "url": "/static/js/main.dcce8594.chunk.js"
+  },
+  {
+    "revision": "f914890b8ef849b53479",
+    "url": "/static/js/runtime~main.952d0879.js"
+  },
+  {
+    "revision": "0d99052b3ab528aad42879e29bc72527",
+    "url": "/static/media/banner-5.0d99052b.png"
+  },
+  {
+    "revision": "6191e9f032ad547c0bd2ca7f02b40c12",
+    "url": "/static/media/box.6191e9f0.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "af4559e89791cd4a8261fa31672439ec",
+    "url": "/static/media/movie.af4559e8.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "f2ac40a7306b6f9db7007acddbeb3ce5",
+    "url": "/static/media/shopping.f2ac40a7.svg"
+  }
+]);
